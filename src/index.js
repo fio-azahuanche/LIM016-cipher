fe013681e7160e2c84732055f1d87aaabb381f4d
unknown <fazahuanchef@gmail.com>
2021-10-04 18:53:52 -0500

    agregar index.html

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+
+botonEmpezar.addEventListener('click', ()=>{
+    document.getElementById("pantalla1").style.display='none';
+    document.getElementById("pantalla2").style.display='block';
+  })
