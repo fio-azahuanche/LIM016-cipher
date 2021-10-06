b95f3bd477bd4836c1fac22ada1a28c840d47961
unknown <fazahuanchef@gmail.com>
2021-10-06 18:24:08 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
-
-/*botonEmpezar.addEventListener('click', ()=>{
+/* Cambio pantalla1 a pantalla2 
+Evento: click en botón Empezar más arrow function*/
+const botonEmpezar = document.getElementById('botonEmpezar');
+botonEmpezar.addEventListener('click', ()=>{
     document.getElementById("pantalla1").style.display='none';
     document.getElementById("pantalla2").style.display='block';
-})*/
+})
+
+
+console.log(cipher);
+
+
+
+
+
+
