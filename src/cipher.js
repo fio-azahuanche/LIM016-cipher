91e43384b59b7174c63be9f785cba816ae8fe5e6
unknown <fazahuanchef@gmail.com>
2021-10-06 18:54:48 -0500

    cifrado mayusculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,4 +1,13 @@
 const cipher = {
+  encode: (string, offset) =>{
+    let mensajeCifrado="";
+    for (let i=0; i<string.length ;i++){
+      let letra = string[i];
+      letra = String.fromCharCode((string.charCodeAt(i)-65+ parseInt(offset))%26+65);
+      mensajeCifrado += letra;
+    }
+    return mensajeCifrado;
+  }
   
 
 };
--- src/index.js
+++ src/index.js
@@ -8,18 +8,14 @@ botonEmpezar.addEventListener('click', ()=>{
     document.getElementById("pantalla2").style.display='block';
 })
 
-let mensajeCifrado="";
-function cifrar(){
-    const botonCifrar = document.getElementById('botonCifrar');
-    let mensajeOriginal = document.getElementById('mensajeOriginal').value;
-    let shift = parseInt(document.getElementById('shift').value);
-    for (let i=0; i<mensajeOriginal.length;i++){
-        mensajeCifrado += String.fromCharCode((mensajeOriginal.charCodeAt(i)-65+shift)%26 +65);
-    }
-    console.log(mensajeCifrado);
-}
-//Evento: click en cifrar
-botonCifrar.addEventListener('click',cifrar,false)
+/* Cifrando el mensaje 
+Evento: click en botón CIFRAR más método obj.encode*/
+const botonCifrar = document.getElementById('botonCifrar');
+botonCifrar.addEventListener('click', ()=>{
+    let mensajeOriginal=document.getElementById('mensajeOriginal').value;
+    let shift = document.getElementById('shift').value;
+    document.getElementById("mensajeCifrado").innerHTML = cipher.encode(mensajeOriginal,shift);
+})
 
 console.log(cipher);
 
