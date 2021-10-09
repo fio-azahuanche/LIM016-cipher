40e0b6f77754abbac12e38d76e00eb52329c312f
unknown <fazahuanchef@gmail.com>
2021-10-09 14:18:57 -0500

    js con mayus minus

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,19 @@
 const cipher = {
   encode: (offset, string) =>{
-    let mensajeCifrado="";
-    for (let i=0; i<string.length ;i++){
-      let letra = string[i];
-      letra = String.fromCharCode((string.charCodeAt(i)-65+ parseInt(offset))%26+65);
-      mensajeCifrado += letra;
-    }
+      let mensajeCifrado="";
+      for (let i=0; i<string.length ;i++){
+        let letra = string[i];
+        if(string.charCodeAt(i)>64 && string.charCodeAt(i)<91){
+          letra = String.fromCharCode((string.charCodeAt(i)-65+ parseInt(offset))%26+65);
+        }
+        else if (string.charCodeAt(i)>96 && string.charCodeAt(i)<123){
+            letra = String.fromCharCode((string.charCodeAt(i)-97+ parseInt(offset))%26+97);
+          }
+        else{
+          letra = string[i];
+        }
+        mensajeCifrado += letra;
+      }
     return mensajeCifrado;
   },
 
@@ -13,7 +21,12 @@ const cipher = {
     let mensajeCifrado="";
     for (let i=0; i<string.length ;i++){
       let letra = string[i];
-      letra = String.fromCharCode((string.charCodeAt(i)+65- parseInt(offset))%26+65);
+      if(string.charCodeAt(i)>64 && string.charCodeAt(i)<91){
+        letra = String.fromCharCode((string.charCodeAt(i)+65- parseInt(offset))%26+65);
+      }
+      else if(string.charCodeAt(i)>96 && string.charCodeAt(i)<123){
+        letra = String.fromCharCode((string.charCodeAt(i)+65- parseInt(offset))%26+97);
+      }
       mensajeCifrado += letra;
     }
     return mensajeCifrado;
